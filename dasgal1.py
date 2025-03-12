72d232b50ce2b778b1c407f65c68794a68ca4df1
sodon <sogii1794@gmail.com>
2025-03-12 14:42:50 +0800

    .

--- dasgal1.py
+++ dasgal1.py
@@ -1,9 +1,9 @@
 ehlel=float(input("too oruul: "))
 if ehlel >= 100:
-    bonus=ehlel + 5
-elif ehlel >100:
-    bonus=ehlel*0.2
-elif ehlel>1000:
-    bonus=ehlel*0.1
-if ehlel%2==0:
-    nemelt=ehlel+1
+    bonus = ehlel + 5
+elif ehlel > 100:
+    bonus = ehlel * 0.2
+elif ehlel > 1000:
+    bonus = ehlel * 0.1
+if ehlel % 2 == 0:
+    nemelt = ehlel + 1
--- dasgal3.py
+++ dasgal3.py
@@ -2,6 +2,6 @@ mar=float(input("markeriin too: "))
 pen=float(input("vzegnii too: "))
 liq=float(input("ugaalgiin nuntag: "))
 dis=float(input("Hunglultiin huwi: ")) /100
-niit=(mar *5.08)+(pen*7.20)+(liq*1.20)
-vne=niit*(1-dis)
+niit=(mar * 5.08) + (pen * 7.20) + (liq * 1.20)
+vne = niit * (1 - dis)
 print(vne)
